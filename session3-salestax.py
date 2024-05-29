f6488472f1b16324ea9955f876fe4151bdf3def0
Kushal Pokhrel <kpblink182@gmail.com>
2024-05-29 19:06:18 +1000

    Create session3-salestax.py
    practice commit first program from soft dev

---
+++ session3-salestax.py
@@ -0,0 +1,15 @@
+# lets assume some xyz company three products
+phones = 1400 # initializing first variable or product
+laptops = 2000 # 20,
+watch = 1500 # 8,
+airpods = 200 # 13,
+tv = 6000 # 8,
+
+st_phones = phones*0.1
+st_laptops = laptops*0.2
+st_watch = watch*0.08
+st_airpods = airpods*0.13
+st_tv = tv*0.08
+
+print(st_phones, st_laptops, st_watch, st_airpods, st_tv)
+print(st_phones+phones, st_laptops+laptops, st_watch+watch, st_airpods+airpods, st_tv+tv)
